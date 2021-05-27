50a742e0bb4e5523654608c2c7583500890c2e7a
Modassir <alimodassir@gmail.com>
2021-05-27 20:10:23 +0530

    Added main website route in TMS sidebar

--- src/TMS/Dashboard/routes.js
+++ src/TMS/Dashboard/routes.js
@@ -2,6 +2,7 @@
 import Dashboard from "@material-ui/icons/Dashboard";
 import Assignment from "@material-ui/icons/Assignment";
 import ContactSupportIcon from "@material-ui/icons/ContactSupport";
+import HomeIcon from "@material-ui/icons/Home";
 
 // core components/views for Admin layout
 import DashboardPage from "./views/Dashboard/Dashboard.js";
@@ -10,6 +11,11 @@ import HelpCenter from "./views/Help Center/HelpCenter";
 import TaskDetail from "./views/Workspace/Components/TaskDetails/TaskDetail";
 
 const dashboardRoutes = [
+  {
+    path: "/",
+    name: "Home",
+    icon: HomeIcon,
+  },
   {
     path: "/dashboard",
     name: "Dashboard",
